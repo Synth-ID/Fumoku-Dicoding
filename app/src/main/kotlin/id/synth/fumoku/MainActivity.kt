f38238f9637ced54826dc5e58a3d9b3fcf6f64a1
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-19 03:02:07 +0700

    Bind activity

--- app/src/main/kotlin/id/synth/fumoku/MainActivity.kt
+++ app/src/main/kotlin/id/synth/fumoku/MainActivity.kt
@@ -2,10 +2,14 @@ package id.synth.fumoku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.synth.fumoku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
