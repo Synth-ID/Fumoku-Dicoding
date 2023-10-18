87d5dd6ab094d7c834c479253f23e4380ecc0def
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-19 06:49:45 +0700

    Replace account circle with avatar

--- app/src/main/kotlin/id/synth/fumoku/MainActivity.kt
+++ app/src/main/kotlin/id/synth/fumoku/MainActivity.kt
@@ -1,7 +1,13 @@
 package id.synth.fumoku
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.content.res.AppCompatResources
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.mikepenz.aboutlibraries.LibsBuilder
 import id.synth.fumoku.databinding.ActivityMainBinding
 
@@ -26,5 +32,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(binding.root)
+
+        Glide.with(this)
+            .load(R.drawable.account)
+            .circleCrop()
+            .into(object : CustomTarget<Drawable>() {
+                private val about: MenuItem = binding.appBar.menu.findItem(R.id.about)
+
+                override fun onResourceReady(icon: Drawable, transition: Transition<in Drawable>?) {
+                    about.icon = icon
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    about.icon = AppCompatResources.getDrawable(
+                        this@MainActivity,
+                        R.drawable.account_circle,
+                    )
+                }
+
+            })
     }
 }
