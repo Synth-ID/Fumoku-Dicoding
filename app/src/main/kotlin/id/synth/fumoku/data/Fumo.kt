f1e83d9f498e1358e90bd74e4768fbc81c23a9cb
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-19 08:00:29 +0700

    Parcelize `Fumo`

--- app/src/main/kotlin/id/synth/fumoku/data/Fumo.kt
+++ app/src/main/kotlin/id/synth/fumoku/data/Fumo.kt
@@ -1,8 +1,12 @@
 package id.synth.fumoku.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Fumo(
     val id: Int,
     val character: String,
     val type: FumoType,
     val name: String,
-)
+) : Parcelable
