952935b391ff380c335383cd96b7e7d694540ab5
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-19 22:29:30 +0700

    Refactor add throws annotation

--- app/src/main/kotlin/id/synth/fumoku/model/TouhouCharacter.kt
+++ app/src/main/kotlin/id/synth/fumoku/model/TouhouCharacter.kt
@@ -1,6 +1,7 @@
 package id.synth.fumoku.model
 
 import androidx.annotation.ColorInt
+import java.net.MalformedURLException
 import java.net.URL
 
 data class TouhouCharacter(
@@ -8,6 +9,9 @@ data class TouhouCharacter(
     val wiki: URL,
     @ColorInt val color: UInt
 ) {
+    @Throws(
+        MalformedURLException::class,
+    )
     constructor(
         name: String,
         wiki: String,
