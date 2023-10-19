9c9f17dc1d565decd90fc6e5b220d6c849426b1b
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-19 08:14:34 +0700

    Use `UInt` color for compatibility and add helper constructor

--- app/src/main/kotlin/id/synth/fumoku/model/TouhouCharacter.kt
+++ app/src/main/kotlin/id/synth/fumoku/model/TouhouCharacter.kt
@@ -1,10 +1,16 @@
 package id.synth.fumoku.model
 
-import android.graphics.Color
+import androidx.annotation.ColorInt
 import java.net.URL
 
 data class TouhouCharacter(
     val name: String,
     val wiki: URL,
-    val color: Color
-)
+    @ColorInt val color: UInt
+) {
+    constructor(
+        name: String,
+        wiki: String,
+        @ColorInt color: UInt
+    ) : this(name, URL(wiki), color)
+}
