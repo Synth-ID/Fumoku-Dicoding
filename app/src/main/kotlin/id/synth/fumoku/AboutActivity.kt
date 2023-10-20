20116f39e78bf19925db1f73ce27643b986e4c5c
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-10-20 09:41:41 +0700

    Basic about layout

--- app/src/main/kotlin/id/synth/fumoku/AboutActivity.kt
+++ app/src/main/kotlin/id/synth/fumoku/AboutActivity.kt
@@ -3,6 +3,7 @@ package id.synth.fumoku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bumptech.glide.Glide
 import id.synth.fumoku.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
@@ -19,5 +20,11 @@ class AboutActivity : AppCompatActivity() {
         }
 
         setContentView(binding.root)
+
+        // Set account avatar from image
+        Glide.with(this)
+            .load(R.drawable.avatar)
+            .circleCrop()
+            .into(binding.avatar)
     }
 }
